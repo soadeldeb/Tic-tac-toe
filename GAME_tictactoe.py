2d0ae0e0229b40345f861186ebf69dbd7660c453
soadeldeb <soadeldeb@gmail.com>
2021-11-10 12:36:02 +0100

    game works

--- GAME_tictactoe.py
+++ GAME_tictactoe.py
@@ -7,6 +7,7 @@
 from random import randint
 import sys, os
 import time
+from operator import itemgetter
 
 
 def menu_main():
@@ -49,7 +50,7 @@ def name_player_checker(player_name, first_p):
     return player_name
 
 
-def player_choise_check(player_choise,board_positions):
+def player_choise_check(player_choise, board_positions):
     # while player_choise.isdigit() == False :
     #     print("\n------------------------------------------------------------------------------")
     #     print('You can only pick a number between 1 and 9')
@@ -65,9 +66,16 @@ def player_choise_check(player_choise,board_positions):
         print("This position is not available, please pick a position that is available on the board ")
         player_choise = input('\nPick a number on the board: ')
     board_positions.remove(int(player_choise))
-    return player_choise,board_positions
+    return player_choise, board_positions
 
-def BoardImage(player_choise,player_turn,Position147,Position258,Position369):
+
+def BoardImage(Position147, Position258, Position369):
+
+    for i in range(len(Position147)):
+        print(Position147[i], Position258[i], Position369[i])
+
+
+def board_update(player_choise, player_turn, Position147, Position258, Position369):
     player_turn += 1
     if player_turn % 2 != 0:
         player = "X"
@@ -75,55 +83,72 @@ def BoardImage(player_choise,player_turn,Position147,Position258,Position369):
         player = "O"
     player_choise = int(player_choise)
 
-
     if player_choise == 1 or player_choise == 4 or player_choise == 7:
         Position147 = [w.replace(str(player_choise), str(player)) for w in Position147]
     if player_choise == 2 or player_choise == 5 or player_choise == 8:
         Position258 = [w.replace(str(player_choise), str(player)) for w in Position258]
     if player_choise == 3 or player_choise == 6 or player_choise == 9:
         Position369 = [w.replace(str(player_choise), str(player)) for w in Position369]
+    return  Position147, Position258, Position369
 
-    for i in range(len(Position147)):
-         print(Position147[i], Position258[i], Position369[i])
 
-    return Position147,Position258,Position369
-
-def boardlogic(player_x,player_o):
+def boardlogic(player_x, player_o):
     board_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    winer == False
+    winner = False
     Position147 = ["    |", " 1  |", "----|", "    |", " 4  |", "----|", "    |", " 7  |", "    |"]
     Position258 = ["    |", " 2  |", "----|", "    |", " 5  |", "----|", "    |", " 8  |", "    |"]
     Position369 = ["    ", "  3 ", "----", "    ", "  6 ", "----", "    ", "  9 ", "    "]
 
-    player_choise = "0"
-    while len(board_positions) != 0:
-        Position147,Position258,Position369 = BoardImage(player_choise,len(board_positions),Position147,Position258,Position369)
-        if len(board_positions) % 2 != 0:
-            print("It is your turn ",player_x , "player X")
-            player_choise = input('\nPick a number on the board: ')
-            checked_player_choise, board_positions = player_choise_check(player_choise,board_positions)
-            winner = check_winner(Position147, Position258, Position369,winner)
+    while len(board_positions) != 0 and winner == False:
 
-        elif len(board_positions) % 2 == 0:
-            print("It is your turn ",player_o , "player O")
+        BoardImage(Position147, Position258, Position369)
+        if len(board_positions) % 2 != 0:
+            print("It is your turn ", player_x, "player X")
             player_choise = input('\nPick a number on the board: ')
             checked_player_choise, board_positions = player_choise_check(player_choise, board_positions)
-            winner = check_winner(Position147, Position258, Position369,winner)
-    BoardImage(player_choise, len(board_positions), Position147, Position258, Position369)
+            Position147, Position258, Position369 = board_update(player_choise, len(board_positions), Position147, Position258, Position369)
+            winner, str_list = check_winner(Position147, Position258, Position369, winner)  # begin en eind ?
+            W_player = player_x
 
 
+        elif len(board_positions) % 2 == 0 and winner == False:
+            winner, str_list = check_winner(Position147, Position258, Position369, winner)
+            print("It is your turn ", player_o, "player O")
+            player_choise = input('\nPick a number on the board: ')
+            checked_player_choise, board_positions = player_choise_check(player_choise, board_positions)
+            Position147, Position258, Position369 = board_update(player_choise, len(board_positions), Position147, Position258,Position369)
+            winner, str_list = check_winner(Position147, Position258, Position369, winner)  # begin en eind ?
+            W_player = player_o
 
-def check_winner(Position147, Position258, Position369):
+        print('\n' *5)
 
-    #postions = Position147 + Position258 + Position369
-    postions = ["    |", " 1  |", "----|", "    |", " X  |", "----|", "    |", " X  |", "    |"]
+    if winner:
+        BoardImage(Position147, Position258, Position369)
+        print("---------------------------------------------------------------------------------------")
+        print("CONGRATS player ", W_player)
+        print('\n' * 5)
+
+    if winner == False:
+        BoardImage(Position147, Position258, Position369)
+        print("it is a draw !")
+
+
+def check_winner(Position147, Position258, Position369, winner):
+    postions = Position147 + Position258 + Position369
     removetable = str.maketrans('', '', '@#%|- ')
     out_list = [s.translate(removetable) for s in postions]
     str_list = list(filter(None, out_list))
-    winning_combo = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    winning_combo =[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    To_remove = ['1', '4', '7', '2', '5', '8', '3', '6', '9']
+    for t in To_remove:
+        for i in range(len(str_list)):
+            str_list[i] = str_list[i].replace(t, "")
+
+
     for i in winning_combo:
-        if all(str_list[i]):
-            return winner == True
+        if all(itemgetter(*i)(str_list)) and ("X" in itemgetter(*i)(str_list) and "O" in itemgetter(*i)(str_list)) == False:
+            winner = True
+    return winner, str_list
 
 
 def main():
@@ -139,17 +164,12 @@ def main():
             player_x = name_player_checker(player_X, "")
             player_O = input('What is your name player O: ')
             player_o = name_player_checker(player_O, player_x)
-            print('\n' * 44)
+            print('\n' * 5)
             print("--------------------------------------------------------------------------------")
             print("Welcome player X : ", player_x, "and player O ", player_o)
             print("--------------------------------------------------------------------------------")
 
-            # functie die per beurt vraagt van de nummer is en checkt of die nummer niet gebruikt is of dat de numer klopt
-            # die functie returnt de correcte nummer en de player aan de beurt en dan wordt de functie van de board aangeroepem
-            # die functie verandert dan de board en print dat mooi uit
-            # dan is weer de andere aan de beurt
-
-            boardlogic(player_x,player_o)
+            boardlogic(player_x, player_o)
 
         if number == 4:
             sys.exit()
